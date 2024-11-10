25c28ec25ad45f04dbad1d63d7323377dda876e4
Michele-Carter <tumeke4u@gmail.com>
2024-11-10 21:22:13 +1300

    added ignore eslint

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,12 @@ const nextConfig = {
     output: 'export',
     typescript: {
         ignoreBuildErrors: true,
-    }
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
+
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
